59cabe6f5eeb9c78ea86fe16cf0665976406d0e7
BeakMinJun <beakminjun92@gmail.com>
2023-12-28 01:01:16 +0900

    [add] 03-10 상수집합

--- jump2java/src/Sample.java
+++ jump2java/src/Sample.java
@@ -1,8 +1,16 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class Sample {
+    // 상수집합
+    enum CoffeeType {
+        AMERICANO,
+        ICE_AMERICANO,
+        CAFE_LATTE
+    };
     public static void main(String[] args){
         System.out.println("********리스트********");
         ArrayList pitches = new ArrayList();
@@ -60,5 +68,59 @@ public class Sample {
         System.out.println(pitches2);
         pitches2.sort(Comparator.reverseOrder());
         System.out.println(pitches2);
+
+        System.out.println("********맵********");
+        HashMap<String, String> map = new HashMap<>();
+        map.put("people", "사람");
+        map.put("baseball", "야구");
+        System.out.println(map.get("people"));
+        System.out.println(map.get("java"));
+        System.out.println(map.getOrDefault("java", "자바"));
+        System.out.println(map.containsKey("people"));
+        // System.out.println(map.remove("people"));
+        // System.out.println(map.getOrDefault("people", "없어용"));
+        System.out.println(map.size());
+        System.out.println(map.keySet());
+        ArrayList<String> keyList = new ArrayList<>(map.keySet());
+        System.out.println(keyList);
+
+        System.out.println("********집합********");
+        HashSet<String> set = new HashSet<>(Arrays.asList("H", "e", "l", "l", "o"));
+        System.out.println(set);
+        HashSet<Integer> s1 = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+        HashSet<Integer> s2 = new HashSet<>(Arrays.asList(4, 5, 6, 7, 8, 9));
+        // 교집합
+        HashSet<Integer> intersection = new HashSet<>(s1);
+        intersection.retainAll(s2);
+        System.out.println(intersection);
+        // 합집합
+        HashSet<Integer> union = new HashSet<>(s1);
+        union.addAll(s2);
+        System.out.println(union);
+        // 차집합
+        HashSet<Integer> substract = new HashSet<>(s1);
+        substract.removeAll(s2);
+        System.out.println(substract);
+        // 집합 자료형과 관련된 메서드
+        System.out.println(set);
+        set.add("Jump");
+        set.add("To");
+        set.add("Java");
+        System.out.println(set);
+
+        set.addAll(Arrays.asList("Python", "C++", "C")); // 값을 한꺼번에 넣기
+        System.out.println(set);
+
+        set.remove("To");
+        System.out.println(set);
+
+        System.out.println("********상수집합********");
+        System.out.println(CoffeeType.AMERICANO);
+        System.out.println(CoffeeType.ICE_AMERICANO);
+        System.out.println(CoffeeType.CAFE_LATTE);
+
+        for(CoffeeType type: CoffeeType.values()) {
+            System.out.println(type);
+        }
     }
 }
